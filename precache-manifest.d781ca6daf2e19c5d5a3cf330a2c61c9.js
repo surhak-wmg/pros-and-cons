cfa143ad1b8ee4eb2a6876cc753111c34cf2b922
Surohak <surhak96@gmail.com>
2021-01-22 14:06:35 +0400

    Updates

---
+++ precache-manifest.d781ca6daf2e19c5d5a3cf330a2c61c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d580913cd31cf89f88e07313d603c8e",
+    "url": "/pros-and-cons/index.html"
+  },
+  {
+    "revision": "8e2d0954d3b4eb91cac2",
+    "url": "/pros-and-cons/static/css/main.d37488b6.chunk.css"
+  },
+  {
+    "revision": "c3fea9307b6c42f5241a",
+    "url": "/pros-and-cons/static/js/2.48f89aa6.chunk.js"
+  },
+  {
+    "revision": "3fdce8e155d3ab59c8b4ce0e22520026",
+    "url": "/pros-and-cons/static/js/2.48f89aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2d0954d3b4eb91cac2",
+    "url": "/pros-and-cons/static/js/main.d2f576fd.chunk.js"
+  },
+  {
+    "revision": "b365499f135349ee9a33",
+    "url": "/pros-and-cons/static/js/runtime-main.a7d91a29.js"
+  },
+  {
+    "revision": "2d0f4c996286167a55604e181184ab4e",
+    "url": "/pros-and-cons/static/media/app_bg.2d0f4c99.webp"
+  },
+  {
+    "revision": "25e91ac679bb999002e3d2b2f422d0b4",
+    "url": "/pros-and-cons/static/media/dark_icon.25e91ac6.svg"
+  },
+  {
+    "revision": "fe4053b9ad1356c378185ae6c36b2845",
+    "url": "/pros-and-cons/static/media/light_icon.fe4053b9.svg"
+  },
+  {
+    "revision": "3bc195ee18abe5847c48a131098c7dbd",
+    "url": "/pros-and-cons/static/media/loading.3bc195ee.svg"
+  },
+  {
+    "revision": "8135ce9ded02281996e2882a927d1782",
+    "url": "/pros-and-cons/static/media/minus.8135ce9d.svg"
+  },
+  {
+    "revision": "65ccc6cd780b71229855557fb0a4dc66",
+    "url": "/pros-and-cons/static/media/plus.65ccc6cd.svg"
+  }
+]);
